62631782835a49ad38675e1e16c2565554ec6c8a
Neeraj Meena IND <114648043+neeraj542@users.noreply.github.com>
2023-07-09 20:26:07 +0530

    Create 1343B.cpp

---
+++ Daily-Codes/1343B.cpp
@@ -0,0 +1,35 @@
+// B. Balanced Array
+// CodeForces: https://codeforces.com/problemset/problem/1343/B
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    #ifdef _DEBUG
+    freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    #endif
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        n /= 2;
+        if (n & 1) {
+            cout << "NO" << endl;
+            continue;
+        }
+        cout << "YES" << endl;
+        for (int i = 1; i <= n; ++i) {
+            cout << i * 2 << " ";
+        }
+        for (int i = 1; i < n; ++i) {
+            cout << i * 2 - 1 << " ";
+        }
+        cout << 3 * n - 1 << endl;
+    }
+    
+    return 0;
+}
